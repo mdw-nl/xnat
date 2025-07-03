767f4bc996956fe6df26eccc622294fbb9c8869d
TomSinsel <sinseltom@gmail.com>
2025-07-03 10:44:16 +0200

    Updated the send_dicom_to_XNAT.py to be compatible with the digione infrastructure

--- sending_xnat_data/send_dicom_to_XNAT.py
+++ sending_xnat_data/send_dicom_to_XNAT.py
@@ -25,26 +25,23 @@ class sendDICOM:
         self.xnat_url = "http://localhost:80"
         self.username = "admin"
         self.password = "admin"
+        self.csv_radiomics = None
         
         
-    def adding_treatment_site(self, patient_ids: list, treatment_sites: list, data_folder):
+    def adding_treatment_site(self, treatment_sites, data_folder):
         """Hardcode the treatment sides where we want filter on in the XNAT projects"""
         try:
             logging.info("Adding a fake treatment site to the dicom files to filter the projects.")
-            
-            for ids, treatment_site, folder in zip(patient_ids, treatment_sites, os.listdir(data_folder)):
-                folder_path = os.path.join(data_folder, folder)
-                
-                for file in os.listdir(folder_path):
-                    if file.endswith(".dcm"):
-                        file_path = os.path.join(folder_path, file)
-                        ds = dcmread(file_path)
-                        ds.PatientID = ids
-                        ds.BodyPartExamined  = treatment_site
-                        ds.save_as(file_path)
+                   
+            for file in os.listdir(data_folder):
+                if file.endswith(".dcm"):
+                    ds = dcmread(file_path)
+                    treatment_site = treatment_sites[ds.PatientID]
+                    file_path = os.path.join(data_folder, file)
+                    ds.BodyPartExamined  = treatment_site
+                    ds.save_as(file_path)
             
             logging.info("added the treatment site")
-            
         except Exception as e:
             logging.error(f"An error occurred adding the fake treatment site: {e}", exc_info=True)
             
@@ -53,44 +50,47 @@ class sendDICOM:
         try: 
             ae = AE()
             ae.requested_contexts = StoragePresentationContexts
-            self.dict_csv_radiomics = {}
-            self.dict_patient_info = {}
-
-            for folder in os.listdir(data_folder):
-                folder_path = os.path.join(data_folder, folder)
+            self.patient_info = []
                 
-                ds = dcmread(os.path.join(folder_path, os.listdir(folder_path)[0]))
-                treatment_site = ds.BodyPartExamined
-                
-                port = ports[treatment_site]["Port"]
-                Title = ports[treatment_site]["Title"]
+            first_iteration = True
             
-                for file in os.listdir(folder_path):
+            for file in os.listdir(data_folder):
+                
+                file_path = os.path.join(data_folder, file)
+                
+                # Get the radiomics csv file from the folder which will be used in upload_csv_to_xnat to upload it
+                if file.endswith(".csv"):
+                    self.csv_radiomics = file_path
+                    logging.info(f"radiomics file found: {file}")    
+                    continue
+            
+                if first_iteration:
+                    ds = dcmread(os.path.join(data_folder, os.listdir(data_folder)[0]))
+                    treatment_site = ds.BodyPartExamined
                     
-                    # Get the radiomics csv file from the folder which will be used in upload_csv_to_xnat to upload it
-                    if file.endswith(".csv"):
-                        self.dict_csv_radiomics[folder] = file
-                        logging.info(f"radiomics file found: {file}")    
-                        continue
+                    port = ports[treatment_site]["Port"]
+                    Title = ports[treatment_site]["Title"]
                     
-                    assoc = ae.associate('localhost', port, ae_title = Title)
-                    file_path = os.path.join(folder_path, file)
-                    
+                    first_iteration = False
+                else:
                     # Get the BodyPartExamined, PatientName, PatientID from the rtstruct, this will be used in upload_csv_to_xnat.
-                    ds = pydicom.dcmread(file_path, stop_before_pixels=True)
-                    if ds.Modality == "RTSTRUCT":
-                        self.dict_patient_info[folder] = [ds.BodyPartExamined, ds.PatientName, ds.PatientID]
-                    
-                    # Send the files to XNAT
-                    if assoc.is_established:
-                        ds = dcmread(file_path)
-                        assoc.send_c_store(ds)
-                    else:
-                        logging.warning("Association failes")
-                    
+                    ds = pydicom.dcmread(file_path, stop_before_pixels=True)                   
+                
+                
+                assoc = ae.associate('localhost', port, ae_title = Title)
+            
+                if ds.Modality == "RTSTRUCT":
+                    self.patient_info = [ds.BodyPartExamined, ds.PatientName, ds.PatientID]
+                
+                # Send the files to XNAT
+                if assoc.is_established:
+                    ds = dcmread(file_path)
+                    assoc.send_c_store(ds)
                     assoc.release()
-                        
-                logging.info("All dicom files send to XNAT")
+                else:
+                    logging.warning("Association failes")
+                    
+            logging.info("All dicom files send to XNAT")
         
         except Exception as e:
             logging.error(f"An error occurred sending dicom files to XNAT: {e}", exc_info=True)
@@ -103,42 +103,48 @@ class sendDICOM:
        
     def upload_csv_to_xnat(self, data_folder):
         """send the radiomics csv to the correct project after the patient dicom data has been send."""
-        try:
-            for folder in self.dict_csv_radiomics:
-                folder_path = os.path.join(data_folder, folder)
-                csv_path = os.path.join(folder_path, self.dict_csv_radiomics[folder])
-                     
-                project, subject, experiment = self.dict_patient_info[folder]
-                check_url = f"{self.xnat_url}/data/projects/{project}/subjects/{subject}/experiments/{experiment}"
-
-                # Check if the dicom files have been archived, only then the CSV files can be send
-                while not self.is_session_ready(check_url):
-                    logging.info("DICOM data is not yet archived, can not send csv")
-                    time.sleep(5)    
-                    
-                upload_url = f"{self.xnat_url}/data/projects/{project}/subjects/{subject}/experiments/{experiment}/resources/csv/files/{self.dict_csv_radiomics[folder]}"
-                logging.info(f"Dicom data archived for session {experiment}, uploading CSV.")
+        
+        # Get the radiomics csv file from the folder if dicom_to_XNAT method has not been used yet
+        if not self.csv_radiomics:
+            for file in os.listdir(data_folder): 
+                file_path = os.path.join(data_folder, file)
                 
-                # Upload the the csv files to XNAT
-                with open(csv_path, 'rb') as f:
-                    response = requests.put(
-                        upload_url,
-                        data=f,
-                        auth=HTTPBasicAuth(self.username, self.password),
-                        headers={'Content-Type': 'text/csv'}
-                    )
-                    
-                    if response.status_code in [200, 201]:
-                        logging.info(f"Uploaded {self.dict_csv_radiomics[folder]} successfully.")
-                    else:
-                        logging.info(f"Failed to upload {self.dict_csv_radiomics[folder]}. Status: {response.status_code}, Error: {response.text}")
+                if file.endswith(".csv"):
+                    self.csv_radiomics = file_path
+                    logging.info(f"radiomics file found: {file}")    
+                    break
+        
+        try:                    
+            project, subject, experiment = self.patient_info
+            check_url = f"{self.xnat_url}/data/projects/{project}/subjects/{subject}/experiments/{experiment}"
+
+            # Check if the dicom files have been archived, only then the CSV files can be send
+            while not self.is_session_ready(check_url):
+                logging.info("DICOM data is not yet archived, can not send csv")
+                time.sleep(5)    
+                
+            upload_url = f"{self.xnat_url}/data/projects/{project}/subjects/{subject}/experiments/{experiment}/resources/csv/files/{self.csv_radiomics}"
+            logging.info(f"Dicom data archived for session {experiment}, uploading CSV.")
+            
+            # Upload the the csv files to XNAT
+            with open(self.csv_radiomics, 'rb') as f:
+                response = requests.put(
+                    upload_url,
+                    data=f,
+                    auth=HTTPBasicAuth(self.username, self.password),
+                    headers={'Content-Type': 'text/csv'}
+                )
+                
+                if response.status_code in [200, 201]:
+                    logging.info(f"Uploaded {self.csv_radiomics} successfully.")
+                else:
+                    logging.info(f"Failed to upload {self.csv_radiomics}. Status: {response.status_code}, Error: {response.text}")
                         
         except Exception as e:
             logging.error(f"An error occurred sending CSV files to XNAT: {e}", exc_info=True)
     
     def run(self, ch, method, properties, body, executor):
-        patient_ids = ["Tom", "Tim"]
-        treatment_sites = ["LUNG", "KIDNEY"]
+        treatment_sites = {"Tom": "LUNG", "Tim": "KIDNEY"}
         ports = {
             "LUNG": {"Title": "LUNG", "Port": 8104},
             "KIDNEY": {"Title": "KIDNEY", "Port": 8104}
@@ -148,7 +154,7 @@ class sendDICOM:
             message_data = json.loads(body.decode("utf-8"))
             data_folder = message_data.get('folder_path')
 
-            self.adding_treatment_site(patient_ids, treatment_sites, data_folder)
+            self.adding_treatment_site(treatment_sites, data_folder)
             self.dicom_to_XNAT(ports, data_folder)
             self.upload_csv_to_xnat(data_folder)
             logging.info(f"Send data from: {data_folder}")
@@ -157,12 +163,12 @@ class sendDICOM:
             logging.error(f"An error occurred in the run method: {e}", exc_info=True)
         
 if __name__ == "__main__":
-    patient_ids = ["Tom", "Tim"]
-    treatment_sites = ["LUNG", "KIDNEY"]
-    ports = {
-        "LUNG": {"Title": "LUNG", "Port": 8104},
-        "KIDNEY": {"Title": "KIDNEY", "Port": 8104}
-    }
+    # patient_ids = ["Tom", "Tim"]
+    # treatment_sites = ["LUNG", "KIDNEY"]
+    # ports = {
+    #     "LUNG": {"Title": "LUNG", "Port": 8104},
+    #     "KIDNEY": {"Title": "KIDNEY", "Port": 8104}
+    # }
     
     # xnat_pipeline = sendDICOM()
     # xnat_pipeline.adding_treatment_site(patient_ids, treatment_sites)
--- sending_xnat_data/send_message.py
+++ sending_xnat_data/send_message.py
@@ -1,8 +1,8 @@
 from config_handler import Config
 from consumer import Consumer
-from send_dicom_to_XNAT import sendDICOM
 
-rabbitMQ_config = Config("rabbitMQxnat")
+
+rabbitMQ_config = Config("xnat")
 cons = Consumer(rmq_config=rabbitMQ_config)
 cons.open_connection_rmq()
 cons.send_message("messages")
