592a7f6dca89e66d689a4423aaa9019c8d508e9a
TomSinsel <sinseltom@gmail.com>
2025-07-10 10:45:25 +0200

    Updated the send_xnat_data

--- sending_xnat_data/send_message.py
+++
@@ -1,8 +0,0 @@
-from config_handler import Config
-from consumer import Consumer
-
-
-rabbitMQ_config = Config("xnat")
-cons = Consumer(rmq_config=rabbitMQ_config)
-cons.open_connection_rmq()
-cons.send_message("messages")
